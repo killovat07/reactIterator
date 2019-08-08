41347feae75511d7fdd0ab73e5e9e39cbf02bb23
Dmitry <dfkillovat@gmail.com>
2019-08-08 13:55:39 +0300

    update counter

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -8,47 +8,40 @@ export default class App extends Component {
     count: 30, //default value
   };
 
-  setOperand(operand, currentCount){
-    switch(operand){
-      case 'increment': 
-        return currentCount+1;
-      case 'decrement': 
-        return currentCount-1;
-      default:
-          return currentCount;
-    }
+  onCounterIncrement = () => {
+    this.setState(({count}) => {  
+      return {count: count+1}
+    });
   };
 
-  onCounterChange = (currentOperand) => {
-    const count = this.setOperand(currentOperand, this.state.count)
-    this.setState({count})
+  onCounterDecrememnt = () => {
+    this.setState(({count}) => {  
+      return {count: count-1}
+    });
   };
-
-  buttons = [
-    { operandName: 'increment', iconName: 'plus', orderCount: 3 },
-    { operandName: 'decrement', iconName: 'minus', orderCount: 1}
-  ];
 
   render(){
     const { count } = this.state;
 
-    const buttons = this.buttons.map(({operandName, iconName, orderCount}) => {
-      return (
-      <button type="button"
-          className={`btn btn-info order-${orderCount}`}
-          key={operandName}
-          onClick={() => this.onCounterChange(operandName)}>
-          <i className={`fa fa-${iconName}`}></i>
-      </button>
-      );
-    });
-
     return (
       <div className="counter-app">
-        <div class="counter-title">Counter</div>
+        <div className="counter-title">Counter</div>
         <div className="counter-block flex-row d-flex align-items-center justify-content-center">
-          <h1 class="order-2"><span class="badge badge-secondary">{count}</span></h1>
-          {buttons}
+
+        <button type="button"
+          className="btn btn-info"
+          onClick={() => this.onCounterDecrememnt()}>
+          <i className="fa fa-minus"></i>
+         </button>
+
+          <h1><span className="badge badge-secondary">{count}</span></h1>
+
+          <button type="button"
+          className="btn btn-info"
+          onClick={() => this.onCounterIncrement()}>
+          <i className="fa fa-plus"></i>
+         </button>
+
         </div>
       </div>
     );
